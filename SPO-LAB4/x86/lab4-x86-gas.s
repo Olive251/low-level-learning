d92756a478bb6e259cca2fc80ed07e5fae99bf6b
Olive251 <oliviataylor437@gmail.com>
2022-11-06 17:54:26 -0500

    cont work on x86 side of lab

--- SPO-LAB4/x86/lab4-x86-gas.s
+++ SPO-LAB4/x86/lab4-x86-gas.s
@@ -3,21 +3,20 @@
 
 min = 0                         /* starting value for the loop index; note that this is a symbol (constant), not a variable */
 max = 30                        /* loop exits when the index hits this number (loop condition is i<max) */
-ten = 10
 
 twoD_output:
     movq     %r15, %rax
     div     %r14        /* divide by 10 to get first digit, puts it in rax*/
                         /* ...quotient is in rdx */
-    add     %rax, $'0'
-    add     %rdx, $'0'
+    add     $'0', %rax
+    add     $'0', %rdx
 
     movq    %rax, %r8
     movq    %rdx, %r9
 
-    lea      $msg_two+5, %r10
+    lea      [msg_two+5], %r10
     movq     %r11, %r10
-    lea      $msg_two+6, %r10
+    lea      [$msg_two+6], %r10
     movq     %r10, %r12
 
     movq     $len_two, %rdx     /* message length */
@@ -26,12 +25,12 @@ twoD_output:
     movq     $1, %rax           /* syscall sys_write */
     syscall
 
-    bl      end
+    jmp      end
 oneD_output:
     movq    %r15, %rax
     
 
-    lea      $msg_one+5, %r10
+    lea      [$msg_one+5], %r10
     movq     %r11, %r10
 
     movq     $len_two, %rdx     /* message length */
@@ -40,7 +39,7 @@ oneD_output:
     movq     $1, %rax           /* syscall sys_write */
     syscall
 
-    bl      end
+    jmp      end
 
 
 _start:
@@ -49,18 +48,17 @@ _start:
     
 determine_digits:
     mov     %r10, %r14
-    cmp     %r10, ten
+    cmp     $10, %r10
     jge twoD_output
 
-    cmp     %r10, ten
+    cmp     $10, %r10
     jl  oneD_output     
 end:
     movq    $0, %rdi    /* exit status */
     movq    $60, %rax   /* syscall sys_exit */
+    syscall
 
-exit:
-
-.data
+.section .data
 msg_one:    .ascii  "Loop #\n"
     len_one=    . - msg_one
 msg_two:    .ascii  "Loop ##\n"
