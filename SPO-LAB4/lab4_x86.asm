88e5f4e951802cc06ceba688c0970cbb9d200d23
Olive251 <oliviataylor437@gmail.com>
2022-11-06 06:19:18 -0500

    began lab4 work

---
+++ SPO-LAB4/lab4_x86.asm
@@ -0,0 +1,19 @@
+.text
+.globl    _start
+
+min = 0                         /* starting value for the loop index; note that this is a symbol (constant), not a variable */
+max = 10                        /* loop exits when the index hits this number (loop condition is i<max) */
+
+_start:
+    mov     $min,%r15           /* loop index */
+
+loop:
+    /* ... body of the loop ... do something useful here ... */
+
+    inc     %r15                /* increment index */
+    cmp     $max,%r15           /* see if we're done */
+    jne     loop                /* loop if we're not */
+
+    mov     $0,%rdi             /* exit status */
+    mov     $60,%rax            /* syscall sys_exit */
+    syscall
